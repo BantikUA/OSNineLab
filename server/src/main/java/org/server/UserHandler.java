3a893744614cc34dfc8562d2b109096f91e7774a
admin <falibogamaks@gmail.com>
2024-11-28 16:12:12 +0200

    creating client and server projects

---
+++ server/src/main/java/org/server/UserHandler.java
@@ -0,0 +1,52 @@
+package org.server;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
+public class UserHandler {
+
+    private final Object lock = new Object();
+
+
+    public boolean isUserNameAvailable(String username) {
+        synchronized (lock) {
+            Scanner scanner = new Scanner(getClass().getResourceAsStream("/users.txt"));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains(username)) {
+                    scanner.close();
+                    return false;
+                }
+            }
+            scanner.close();
+            return true;
+        }
+    }
+
+    public void register(String username, String password) throws URISyntaxException, IOException {
+        synchronized (lock) {
+            var file = new File(getClass().getResource("/users.txt").toURI());
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(username + " " + password);
+            fileWriter.close();
+        }
+    }
+
+    public boolean login(String username, String password) {
+        synchronized (lock) {
+            Scanner scanner = new Scanner(getClass().getResourceAsStream("/users.txt"));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equals(username + " " + password)) {
+                    scanner.close();
+                    return true;
+                }
+            }
+            scanner.close();
+            return false;
+        }
+    }
+}
