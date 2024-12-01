0de67a9a65191194a7105a42166d702e99db6682
Zakhar <zakhar.semenyuk@gmail.com>
2024-12-01 16:48:13 +0200

    Old authorize fxml-form

--- client/src/main/java/org/client/HelloApplication.java
+++ client/src/main/java/org/client/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("authorize.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Client");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
---
+++ client/src/main/java/org/client/authorizeController.java
@@ -0,0 +1,22 @@
+package org.client;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class authorizeController {
+    public Label errorMsgLogIn;
+    public TextField loginFieldSignUp;
+    public PasswordField passwordFieldSignUp;
+    public Label errorMsgSignUp;
+    public TextField loginFieldLogIn;
+    public PasswordField passwordFieldLogIn;
+
+    public void onSignUpButtonClick(ActionEvent event) {
+
+    }
+
+    public void onLogInButtonClick(ActionEvent event) {
+    }
+}
